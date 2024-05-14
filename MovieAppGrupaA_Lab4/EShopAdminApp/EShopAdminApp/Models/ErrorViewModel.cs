89fe4af1d09b632dd7626167b09646b2fd295de5
Hristina Sekuloska <162374170+Sekuloska@users.noreply.github.com>
2024-05-15 00:40:02 +0200

    Delete MovieAppGrupaA_Lab4 directory

--- MovieAppGrupaA_Lab4/EShopAdminApp/EShopAdminApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EShopAdminApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
